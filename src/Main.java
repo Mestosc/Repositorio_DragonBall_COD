5d5e2ebb3475ccd4c7f1576f606a55a076653938
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-07 09:20:41 +0100

    Añadiendo nuevo personaje al main

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Dragon Ball!");
         Personaje personaje1 = new Personaje();
+        System.out.println("Hola " + personaje1.nombre);
+        Personaje personaje2 = new Personaje("Goku");
     }
 }
