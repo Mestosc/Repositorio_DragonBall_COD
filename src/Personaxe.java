cbb433db11de9d666a9d0132c31c9d66b18add2a
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-07 08:59:01 +0100

    Documentando constructor

--- src/Personaxe.java
+++ src/Personaxe.java
@@ -2,6 +2,11 @@ public class Personaxe {
     String nombre;
     int vida;
 
+    /**
+     * Constructor de los personajes que permite definir el nombre y la vida por defecto
+     * @param nombre el nombre del personaje
+     * @param vida la vida del personaje
+     */
     public Personaxe(String nombre,int vida) {
         this.nombre = nombre;
         this.vida = vida;
