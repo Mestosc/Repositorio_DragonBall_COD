96a790ffddc15892cb1dad2078f578f267b42501
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-12 13:51:07 +0100

    Adding tests for method perderVida

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Dragon Ball!");
-        Personaje personaje1 = new Personaje();
-        System.out.println("Hola " + personaje1.getNombre());
-        Personaje personaje2 = new Personaje("Goku");
-        System.out.println(personaje2.getNombre() + personaje2.perderVida(3));
+        Personaje personaje1 = new Personaje("Goku",-12);
+        Personaje personaje2 = new Personaje("Gokud",1000);
+        Personaje personaje3 = new Personaje("Goku",0);
+        Personaje personaje4 = new Personaje("Goku",100);
+        System.out.println(personaje1.perderVida(2000));
+        System.out.println(personaje2.perderVida(4000));
+        System.out.println(personaje4.perderVida(-1000));
+        System.out.println(personaje3.perderVida(-231));
     }
 }
