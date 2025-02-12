485a753ce18441f5f48f38b2e94bbd30a6d24982
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-12 13:39:01 +0100

    Testing in the Main.java the functionalites

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Dragon Ball!");
         Personaje personaje1 = new Personaje();
-        System.out.println("Hola " + personaje1.nombre);
+        System.out.println("Hola " + personaje1.getNombre());
         Personaje personaje2 = new Personaje("Goku");
+        System.out.println(personaje2.getNombre() + personaje2.perderVida(3));
     }
 }
