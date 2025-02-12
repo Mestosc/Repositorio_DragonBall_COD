be793fc88c803f0e6887a611e315856cb0f0c49c
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-12 14:07:06 +0100

    Giving a good format to Tests

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,15 @@ public class Main {
         Personaje personaje2 = new Personaje("Gokud",1000);
         Personaje personaje3 = new Personaje("Goku",0);
         Personaje personaje4 = new Personaje("Goku",100);
-        System.out.println(personaje1.perderVida(2000));
+        // Test1
+        int vidaPerdida = personaje1.perderVida(-2000);
+        if (vidaPerdida==2012)  System.out.println("Ok");
+        else System.out.println("Fallo");
+        // Test2
+        vidaPerdida = personaje2.perderVida(-2000);
+        if (vidaPerdida==1000)  System.out.println("Ok");
+        else System.out.println("Fallo");
+        // Test3
         System.out.println(personaje2.perderVida(4000));
         System.out.println(personaje4.perderVida(-1000));
         System.out.println(personaje3.perderVida(-231));
